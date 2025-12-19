a26fa2e8fca4124e82c231686cbd46a886912bfc
dnadubey <24.aditya.dubey@gmail.com>
2025-12-19 17:34:29 +0530

    first commit

--- controller/reviewController.js
+++ controller/reviewController.js
@@ -2,18 +2,24 @@ const Review=require('./../modal/reviewModel')
 
 exports.submitReview=async (req, res) => {
   try {
-    const review = new Review({
-      companyId: req.body.companyId,
-      name: req.body.name,
-      mobile: req.body.mobile,
-      email: req.body.email,
-      rating: req.body.rating,
-      comment: req.body.comment
-    });
+    // const review = new Review({
+    //   companyId: req.body.companyId,
+    //   name: req.body.name,
+    //   mobile: req.body.mobile,
+    //   email: req.body.email,
+    //   rating: req.body.rating,
+    //   comment: req.body.comment
+    // });
 
-    await review.save();
+    // await review.save();
 
-    res.send("<h2 style='text-align:center'>Thank you for your review ⭐</h2>");
+    const data= await Review.create(req.body);
+    res.status(200).json({
+        status:"Success",
+        data:data
+    })
+
+    //res.send("<h2 style='text-align:center'>Thank you for your review ⭐</h2>");
   } catch (error) {
     console.error(error);
     res.status(500).send("Failed to submit review");
--- modal/reviewModel.js
+++ modal/reviewModel.js
@@ -47,4 +47,4 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
-module.exports = mongoose.model("Review", reviewSchema);
+module.exports = mongoose.model("review", reviewSchema);
