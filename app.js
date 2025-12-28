67c021658fb0d567bc7a4eee7a53c08fbddb6f3d
dnadubey <24.aditya.dubey@gmail.com>
2025-12-29 01:13:22 +0530

    first commit

--- app.js
+++ app.js
@@ -24,9 +24,8 @@ mongoose.connect(DB,).then((con)=>{
   console.log("DB connection successfull");
   });
 
-
-// app.use('/api/v1',companyRouter);
-app.use('/api/v1',compaanyWithImageRouter);
+ app.use('/api/v1',companyRouter);
+//app.use('/api/v1',compaanyWithImageRouter);
 app.use('/api/v1/review',reviewRouter);
 app.get('/review/:companyId', (req, res) => {
   res.sendFile(path.join(__dirname, 'review.html'));
